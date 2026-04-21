34d50d544430aac17ab3766a8afe27bf1bce7823
Ayoyemi Ajisebutu <yemi@primebytes.ca>
2026-04-21 12:04:54 -0400

    Replaced the Blackboard text from the HTML template and replaced with Twixt and also the URIs

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,15 +1,15 @@
 # -*- encoding: utf-8 -*-
 
-bind = '0.0.0.0:6500'
+bind = "unix:/run/gunicorn/lmx.sock"
 workers = 3
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
 # Added by Prime
-command= '/home/django/blackboard/env/bin/gunicorn'
-pythonpath='/home/django/blackboard'
+command= '/home/django/lmx/env/bin/gunicorn'
+pythonpath='/home/django/lmx'
 
 # Server settings
-errorlog = "/home/django/blackboard/gunicorn.error"
-accesslog = "/home/django/blackboard/gunicorn.access"
+errorlog = "/home/django/lmx/gunicorn.error"
+accesslog = "/home/django/lmx/gunicorn.access"
